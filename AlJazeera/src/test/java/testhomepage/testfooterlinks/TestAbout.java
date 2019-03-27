68429dd07c07bef5109929e01a830fd63de60a2a
Anika Sian <aetsian@gmail.com>
2019-03-26 21:47:15 -0400

    Created test cases for 'Our Channels' and 'Our Network'

--- AlJazeera/src/test/java/testhomepage/testfooterlinks/TestAbout.java
+++ AlJazeera/src/test/java/testhomepage/testfooterlinks/TestAbout.java
@@ -1,5 +1,161 @@
 package testhomepage.testfooterlinks;
 
-public class TestAbout {
+import base.CommonAPI;
+import homepage.footerlinks.About;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import reporting.ApplicationLog;
+import reporting.TestLogger;
 
+public class TestAbout extends CommonAPI {
+   About newAbout;
+
+   @BeforeMethod
+   public void init() {
+      newAbout = PageFactory.initElements(driver, About.class);
+   }
+
+   @Test
+   // @Test(priority = 55)
+   public void clickOnAboutUs() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickAboutUs();
+   }
+
+   @Test
+   // @Test(priority = 56)
+   public void clickOnPressOffice() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickPressOffice();
+   }
+
+   @Test
+   // @Test(priority = 57)
+   public void clickOnAwards() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickAwards();
+   }
+
+   @Test
+   // @Test(priority = 58)
+   public void clickOnCodeOfEthics() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickCodeOfEthics();
+   }
+
+   @Test
+   // @Test(priority = 59)
+   public void clickOnTermsAndCond() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickTermsAndCond();
+   }
+
+   @Test
+   // @Test(priority = 60)
+   public void clickOnPrivacyPolicy() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickPrivacyPolicy();
+   }
+
+   @Test
+   // @Test(priority = 61)
+   public void clickOnCookiePolicy() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickCookiePolicy();
+   }
+
+   @Test
+   // @Test(priority = 62)
+   public void clickOnCookiePref() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickCookiePref();
+   }
+
+   @Test
+   // @Test(priority = 63)
+   public void clickOnCommGuidelines() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickCommGuidelines();
+   }
+
+   @Test
+   // @Test(priority = 64)
+   public void clickOnWorkForUs() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickWorkForUs();
+   }
+
+   @Test
+   // @Test(priority = 65)
+   public void clickOnHRQuality() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newAbout.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newAbout.scrollToFooterField();
+      newAbout.clickHRQuality();
+   }
 }
--- AlJazeera/src/test/java/testhomepage/testfooterlinks/TestOurChannels.java
+++ AlJazeera/src/test/java/testhomepage/testfooterlinks/TestOurChannels.java
@@ -1,4 +1,96 @@
 package testhomepage.testfooterlinks;
 
-public class TestOurChannels {
+import base.CommonAPI;
+import homepage.footerlinks.OurChannels;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import reporting.ApplicationLog;
+import reporting.TestLogger;
+
+public class TestOurChannels extends CommonAPI {
+   OurChannels newChannels;
+
+   @BeforeMethod
+   public void init() {
+      newChannels = PageFactory.initElements(driver, OurChannels.class);
+   }
+
+   @Test
+   // @Test(priority = 78)
+   public void clickOnArabic() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newChannels.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newChannels.scrollToFooterField();
+      newChannels.clickArabic();
+   }
+
+   @Test
+   // @Test(priority = 79)
+   public void clickOnEnglish() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newChannels.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newChannels.scrollToFooterField();
+      newChannels.clickEnglish();
+   }
+
+   @Test
+   // @Test(priority = 80)
+   public void clickOnMubasher() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newChannels.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newChannels.scrollToFooterField();
+      newChannels.clickMubasher();
+   }
+
+   @Test
+   // @Test(priority = 81)
+   public void clickOnDocumentary() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newChannels.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newChannels.scrollToFooterField();
+      newChannels.clickDocs();
+   }
+
+   @Test
+   // @Test(priority = 82)
+   public void clickOnBalkans() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newChannels.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newChannels.scrollToFooterField();
+      newChannels.clickBalkans();
+   }
+
+   @Test
+   // @Test(priority = 83)
+   public void clickOnAJPlus() throws Exception {
+      ApplicationLog.epicLogger();
+      TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+      }.getClass().getEnclosingMethod().getName()));
+
+      newChannels.closeCookiePopup();
+      // Icon is not visible to click. Must scroll down.
+      newChannels.scrollToFooterField();
+      newChannels.clickAJPlus();
+   }
 }
